c733842873c4c134d40d9c39a12fcb493396573c
West Kraemer <kraemerwest@gmail.com>
2017-08-18 13:27:30 -0400

    add articles in blog to core data

--- blogAPI/MasterViewController.swift
+++ blogAPI/MasterViewController.swift
@@ -37,6 +37,37 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
                         
                         print(jsonResult)
                         
+                        if let items = jsonResult["items"] as? NSArray {
+                            
+                            for item in items as [AnyObject] {
+                                
+                                print(item["published"])
+                                print(item["title"])
+                                print(item["content"])
+                                
+                                let context = self.fetchedResultsController.managedObjectContext
+                                let newEvent = Event(context: context)
+                                
+                                // If appropriate, configure the new managed object.
+                                newEvent.timestamp = NSDate()
+                                newEvent.setValue(item["published"] as! String, forKey: "published")
+                                newEvent.setValue(item["title"] as! String, forKey: "title")
+                                newEvent.setValue(item["content"] as! String, forKey: "content")
+                                
+                                // Save the context.
+                                do {
+                                    try context.save()
+                                } catch {
+                                    // Replace this implementation with code to handle the error appropriately.
+                                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                                    let nserror = error as NSError
+                                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                                }
+                                
+                            }
+                            
+                            self.tableView.reloadData()                        }
+                        
                     } catch {
                         
                         print("JSON Processing Failed")
